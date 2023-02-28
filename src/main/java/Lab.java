071b08eb59b991b41491162382f8307137b8228c
Andre Gonzales <gonzales.andre@gmail.com>
2023-02-28 18:45:04 +0000

    Module wouldn't mark complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -4,6 +4,7 @@ public class Lab {
      * This method should return whatever String was passed into it by whatever invoked it (in this case, the test),
      * instead of returning "return text please!"
      *
+     * 
      * for example:
      * String str = copy("hello");
      * System.out.println(str); -> prints out "hello"
